fc2a721c839c9a361ea36e1a2e18148e88c651c5
ellijs <ellijs@hotmail.com>
2021-07-17 23:52:18 -0400

    Completed Assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,11 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  const style = {color : "firebrick"}
+  return( 
+    <div id="home">
+      <h1 style={style}> {name} is a Web Developer from {city}</h1>
+    </div>)
 }
 
 export default Home;
